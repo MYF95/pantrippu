d9bf29419ec70167b6b22e7670d458f43ead707d
Mack Yao Fu <mack@secret-source.eu>
2018-02-22 14:03:25 +0000

    full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns full title page
+  def full_title(page_title = '')
+    base_title = "Pantrippu"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
